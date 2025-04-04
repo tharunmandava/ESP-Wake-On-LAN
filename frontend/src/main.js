62971c89a6f09284838dbdd3da75a6bd5ebb5c0a
whizzkid <tharunmandava@gmail.com>
2025-04-04 14:46:10 +0000

    initializing localStorage

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,27 +7,11 @@ const VITE_URL = import.meta.env.VITE_URL;
 
 console.log(`${VITE_URL}/status`);
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//       <img src="${viteLogo}" id="toggleLogo" class="logo" alt="Vite logo" />
-//     <h1>Hello Whizzkid!</h1>
-//         <div class="card">
-//       <button id="yes" type="button">yes</button>
-//       <button id="no" type="button">no</button>
-//     </div>
-//     <p class="status-text">
-//     Status
-//     </p>
-//     <p class="status-text" id="status-text">
-//       Placeholder
-//     </p>
-//     <div id="passwordScreen">
-//       <input type="password"/> 
-//       <button id="save">save</button>
-//     </div>
+document.getElementById('save').addEventListener('click', () =>{
+  const passwordInput = document.getElementById('password');
+  localStorage.setItem("secret","passwordInput");
 
-//   </div>
-// `
+})
 
 document.getElementById('toggleLogo').addEventListener('click', () => {
   document.getElementById("passwordScreen").classList.toggle('hidden');
